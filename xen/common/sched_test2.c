24808113d8e48a32d05f03c251b58ab8e619be92
Yunxiang Zhang <davidzhang0717@gmail.com>
2018-04-26 15:09:13 -0500

    quick update

--- xen/common/sched_test2.c
+++ xen/common/sched_test2.c
@@ -1408,7 +1408,7 @@ static void put_prev_entity(struct cfs_rq *cfs_rq, struct sched_entity *prev)
 	 * If still on the runqueue then deactivate_task()
 	 * was not called and update_curr() has to be done:
 	 */
-	if (prev->on_rq)
+	//if (prev->on_rq)
 		update_curr(cfs_rq);
 
 	/* throttle cfs_rqs exceeding runtime */
@@ -1416,13 +1416,13 @@ static void put_prev_entity(struct cfs_rq *cfs_rq, struct sched_entity *prev)
 
 	check_spread(cfs_rq, prev);
 
-	if (prev->on_rq) {
+	//if (prev->on_rq) {
 		//update_stats_wait_start(cfs_rq, prev);
 		/* Put 'current' back into the tree. */
 		__enqueue_entity(cfs_rq, prev);
 		/* in !on_rq case, update occurred at dequeue */
 		update_load_avg(cfs_rq, prev, 0);
-	}
+	//}
 	cfs_rq->curr = NULL;
 }
 
@@ -3797,7 +3797,7 @@ csched_schedule(
     {
         snext = scurr;
         snext->start_time += now;
-        //yield_task_fair();
+        yield_task_fair(cfs_rq_of(snext));
         perfc_incr(delay_ms);
         /*
          * Next timeslice must last just until we'll have executed for
